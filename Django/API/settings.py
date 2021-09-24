db6aee83ddc7c02299e49e5eec33c85a5d394186
ericadiasdematos <erica.diasmatos99@gmail.com>
2021-09-24 14:44:55 +0200

    hey

--- Django/API/settings.py
+++ Django/API/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'efreiAPI.apps.EfreiapiConfig',
+    'efreiAPI.apps.EfreiAPIConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
