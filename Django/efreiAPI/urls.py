49fd3c8b311a0d8a1bb0c12fff3851f63259fc89
Tayssir Younsi <tayssir.younsi@etu.univ-lehavre.fr>
2021-09-24 19:23:03 +0200

    commit views.py + urls.py + serializers.py

--- Django/efreiAPI/urls.py
+++ Django/efreiAPI/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^api/efreiAPI$', views.Livre_list_published),
     url(r'^api/addEfreiAPI$', views.add_livre),
     url(r'^api/efreiAPI/(?P<pk>[0-9]+)$', views.delete_livre),
+    url(r'^api/PutEfreiAPI/(?P<pk>[0-9]+)$', views.update_livre),
 ]
--- Django/efreiAPI/views.py
+++ Django/efreiAPI/views.py
@@ -28,14 +28,29 @@ def add_livre(request):
         return JsonResponse(livre_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['PUT'])
-# def add_livre():
-#    if request.method == 'PUT':
-#        return None
+@api_view(['PUT'])
+def update_livre(request, pk):
+    try:
+
+        livre = Livre.objects.get(pk=pk)
+
+    except Tutorial.DoesNotExist:
+
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+
+        livre_data = JSONParser().parse(request)
+        livre_serializer = LivreSerializer(livre, data=livre_data)
+        if livre_serializer.is_valid():
+            livre_serializer.save()
+            return JsonResponse(livre_serializer.data)
+        return JsonResponse(livre_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['DELETE'])
 def delete_livre(request, pk):
+
     try:
 
         livre = Livre.objects.get(pk=pk)
@@ -45,5 +60,6 @@ def delete_livre(request, pk):
         return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'DELETE':
+
         livre.delete()
         return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
